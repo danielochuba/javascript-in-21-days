7cf67c0fddba53e97ac3140c134a50e3a7f64dfa
Daniel Ochuba <danielochuba78@gmail.com>
2024-01-10 22:22:06 +0100

    learn control flow using if statement

---
+++ day3.js
@@ -0,0 +1,11 @@
+// CONTROL FLOW (IF SSTATEMWNTS)
+
+let num1 = 23;
+
+function oddEven(num) {
+    if (num % 2 == 0) {
+        console.log(`${num} is an even number`)
+    } else {
+    console.log(`${num} is an odd number`)
+    }
+}
